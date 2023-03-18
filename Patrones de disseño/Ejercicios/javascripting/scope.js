74accb34ec6cef6dacae10efc20aa37e8b086e29
ArgesMtz <128236238+ArgesMtz@users.noreply.github.com>
2023-03-18 09:30:34 -0600

    Update scope.js

--- Patrones de disseño/Ejercicios/javascripting/scope.js
+++ Patrones de disseño/Ejercicios/javascripting/scope.js
@@ -10,10 +10,11 @@ const a = 1; const b = 2; const c = 3;
       const a = 7; const c = 9;
 
       (function fourthFunction () {
-        const a = 1; const c = 6;
+        const a = 1; const c = 8;
         console.log(`a: ${a}, b: ${b}, c: ${c}`); 
        
       })()
     })()
+     console.log(`a: ${a}, b: ${b}, c: ${c}`); 
   })()
 })()
